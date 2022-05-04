adc52c3f4f285ca889beec99d3b435486dfe782c
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-04 09:51:52 +0100

    first commit

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = Vue.createApp({
             email: '1fake@gmail.com',
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/83.jpg'
+        }, {
+            firstname: 'Lyia',
+            lastname: 'Marshall',
+            email: '2fake@gmail.com',
+            gender: 'female',
+            picture: 'https://randomuser.me/api/portraits/women/89.jpg'
         }
     }
 })
